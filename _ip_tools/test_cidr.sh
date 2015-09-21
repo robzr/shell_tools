d38858fc5fece68116df2e878f734f274e41fda7
Rob Zwissler <robzr@kavi.com>
2015-09-21 16:18:51 -0700

    IP Tools CIDR manipulation

--- _ip_tools/test_cidr.sh
+++ _ip_tools/test_cidr.sh
@@ -3,22 +3,32 @@
 # Arg1 = network (in cidr or ip/netmask)
 # Arg2 = IP to compare/test [optional]
 
+[ -z "$1" ] && { 
+  cat <<-_EOF_
+	Usage: $0 network [ip]
+	  network is in CIDR or old-school network/sub.n.e.t format
+	  if ip is supplied, it will be compared to the network
+	_EOF_
+  exit -1
+}
+
 . _ip_tools.inc
 
-binstring=`_ipt_IPToBinstring $1`
-echo binstring: $binstring
-_ipt_type $binstring
-echo back to IP: `_ipt_binstringToIP $binstring`
+echo Detected input type: `_ipt_type "$1"`
 
-echo Input type: `_ipt_type "$1"`
-echo Network netmask: `_ipt_getNM $1`
-echo Network binstring: `_ipt_IPToBinstring $1`
-##echo Network binstring with netmask applied: $net_binstring_netmask_applied
-##net_binstring_netmask_applied=`_ipt_binstringApplyNM $net_binstring $net_netmask`
+binstring=`_ipt_IPToBinstring $1`
+
+echo Converted to binstring: $binstring
+
+echo Converted binstring back to IP: `_ipt_binstringToIP $binstring`
+
+netmask=`_ipt_getNM $1`
+echo Extracting netmask: $netmask
+
+net_binstring_netmask_applied=`_ipt_binstringApplyNM $binstring $netmask`
+echo Network binstring with netmask applied: $net_binstring_netmask_applied
 
 if [ -n "$2" ] ; then
-  echo 'IP binstring: '`_ipt_IPToBinstring $2`
-  echo -n "IP ($2) in net $1: "
-  _ipt_isInNet $2 $1 && echo Yes || echo No
+  echo -n "Is IP ($2) in net $1: "
+  _ipt_isInNet "$1" "$2"  && echo Yes || echo No
 fi
-
